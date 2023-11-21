b0067d2b891d1e792484ff57c8df92370b0e5e06
ri-bhatta <57339760+ri-bhatta@users.noreply.github.com>
2023-11-21 12:40:36 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+class MainClass:
+    def __init__(self):
+        # Constructor code goes here
+        pass
+
+    def main_method(self):
+        # Main method code goes here
+        print("Hello from the main method!")
+
+# Entry point of the program
+if __name__ == "__main__":
+    # Creating an instance of MainClass
+    main_instance = MainClass()
+
+    # Calling the main method
+    main_instance.main_method()
